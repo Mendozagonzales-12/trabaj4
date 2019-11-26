2f9e4812ec4b946d08dbe954721dad88a507d79d
Mendozagonzales-12 <57878122+Mendozagonzales-12@users.noreply.github.com>
2019-11-26 18:34:13 -0500

    se agrego los conversores de mendoza

---
+++ mendoza/CONVERSORES.py
@@ -0,0 +1,26 @@
+#Conversores
+
+#1Convertir el entero 569 en cadena
+x=569
+g=str(x)  #str(569)
+print(g, type(g))
+
+#2Convertir la cadena "-9024" en entero
+y= "-9024"
+d= int(y) # int("-9024")
+print (d, type(d))
+
+#3Convertir el entero 38905 en flotante
+w=38905
+a= float(w) # float(38905)
+print (a, type(a))
+
+#4Convertir el flotante 48,71 en entero
+z=48,71
+b= int(z) # int(48,71)
+print (b, type(b))
+
+#5Convertir el flotante - 2590,13 en cadena
+v=-2590,13
+c= str(v) # str(-2590,13)
+print (c, type(c))
