057ee006664bb85f034c425a8ea015fc49340bae
Mendozagonzales-12 <57878122+Mendozagonzales-12@users.noreply.github.com>
2019-11-26 18:31:57 -0500

    se agrego los conversores de damian

---
+++ damian/conversoresM.py
@@ -0,0 +1,25 @@
+#Conversores
+
+#Convertir el flotante  36,25 en cadena
+p=-36,25
+r= str(p) # str(36,25)
+print (r, type(r))
+
+#Convertir el entero 180 a real
+x=180
+g=float(x)  #float(180)
+print(g, type(g))
+
+#Convertir la cadena "73708778" en entero
+y= "73708778"
+c= int(y) # int("73708778")
+print (c, type(c))
+#Convertir el flotante 27,4 en entero
+u=27,4
+o= int(u) # int(48,71)
+print (o type(o))
+
+#Convertir el entero 2767 en flotante
+k=2767
+h= float(k) # float(2767)
+print (h, type(h))
